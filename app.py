7b9d46c7f622b5ab21588ca1f2e928cfdf6c015c
RupinVijan <rupinvijan@gmail.com>
2022-04-15 19:56:03 +0530

    cors error resolved

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify,request
 from textblob import TextBlob
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/spellcheck" ,methods=['GET', 'POST'])
 def index():
